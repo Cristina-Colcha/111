f3bcac7d8722c72357acd1ad0bd2d29571469d7a
Cristina-Colcha <criscolcha@gmail.com>
2025-02-09 15:03:33 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/calculator');
-    console.log('MongoDB connected');
+    await mongoose.connect('mongodb://admin:1751404730@98.82.9.250:27017/calculator?authSource=admin', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDB connected to EC2');
   } catch (error) {
     console.error('Error connecting to MongoDB', error);
     process.exit(1);
